a5673095d8a8efb967c69bb96545deb7eca1a3f6
Jennifer <jenniferpeterson@pursuit.org>
2024-03-04 10:55:31 -0500

    set up my DELETE request but need to fix my total color functionality

--- src/TransactionIndex.jsx
+++ src/TransactionIndex.jsx
@@ -1,33 +1,52 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./TransactionIndex.css";
+// import { useParams } from "react-router-dom";
+const TransactionIndex = ({
+  transactions,
+  setTransactions,
+  humanReadableDate,
+}) => {
+  const [totalBalance, setTotalBalance] = useState(0);
 
-const Home = ({ transactions, humanReadableDate }) => {
-  // console.log(transactions.map((transaction) => transaction.item_name));
+  let totalColor = "";
+  if (totalBalance <= 100) {
+    totalColor = "green";
+  } else if (totalBalance > 100) {
+    totalColor = "yellow";
+  } else {
+    totalColor = "red";
+  }
 
-  // function for the text colors
-  const color = (total) => {
-    if (total > 0) {
-      return "green";
-    } else {
-      return "red";
-    }
-  };
   // function for the total
   const total = transactions
     .map((transaction) => +transaction.amount)
     .reduce((acc, curr) => acc + curr, 0);
 
+  const handleDelete = (id) => {
+    const options = {
+      method: "DELETE",
+    };
+    fetch(`http://localhost:4444/transactions/${id}`, options)
+      .then((res) => res.json())
+      .then((data) => setTransactions(data.transactions));
+  };
+  if (!transactions.length) return null;
   return (
     <div>
-      <h2>Bank Account Total: {total} </h2>
-      {transactions.map((transaction) => (
-        <div key={transaction.id}>
-          <p>{humanReadableDate(transaction.date)}</p>
-          {/* I think i can nest the item name bnetween a link tag for the details page */}
-          <Link to={`/transactions/${transaction.id}`}>
-            <p>{transaction.name}</p>
+      <h2>
+        Bank Account Total:
+        <span className={`${totalColor}`}>{+total}</span>
+      </h2>
+      {transactions.map(({ id, date, name, amount }) => (
+        <div key={id}>
+          <p>{humanReadableDate(date)}</p>
+          {/* I think i can nest the item name between a link tag for the details page */}
+          <Link to={`/transactions/${id}`}>
+            <p>{name}</p>
           </Link>
-          <p>{transaction.amount} </p>
-
+          <p>{amount} </p>
+          <button onClick={() => handleDelete(id)}>Delete</button>
           <hr />
         </div>
       ))}
@@ -35,4 +54,4 @@ const Home = ({ transactions, humanReadableDate }) => {
   );
 };
 
-export default Home;
+export default TransactionIndex;
